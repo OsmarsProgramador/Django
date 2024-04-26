7b41a9630d4189526ea1a9ee2a7c0bbd2d0a7da7
OsmarsProgramador <123673084+OsmarsProgramador@users.noreply.github.com>
2024-04-26 19:55:30 -0300

    Commit
    Atualização

--- W3/MeuPrimeiroProjeto/appmembers/models.py
+++ W3/MeuPrimeiroProjeto/appmembers/models.py
@@ -1,3 +1,22 @@
+"""from django.db import models
+
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+"""
+
+'''from django.db import models
+
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  phone = models.IntegerField()
+  joined_date = models.DateField()'''
+
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  phone = models.IntegerField(null=True)
+  joined_date = models.DateField(null=True)
--- W3/MeuPrimeiroProjeto/appmembers/urls.py
+++ W3/MeuPrimeiroProjeto/appmembers/urls.py
@@ -5,4 +5,11 @@ from . import views
 # criando uma URL que aponte para uma view chamada members
 urlpatterns = [
     path('appmembers/', views.members, name='members'),
+    path('appmembers/details/<int:id>', views.details, name='details'),
 ]
+
+"""
+'appmembers/' representa o caminho da URL que será acessado no navegador.
+views.members é a view que será chamada quando essa URL for acessada.
+'members' é o nome atribuído a essa URL, que pode ser usado para referenciar a URL em templates ou em outras partes do código.
+"""
