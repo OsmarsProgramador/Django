4f3eaea38b3d73a0944d4a902d7fc3e0c3d451f3
OsmarsProgramador <123673084+OsmarsProgramador@users.noreply.github.com>
2024-04-28 13:39:33 -0300

    Atualiza

--- W3/ADMIN_DJANGO/core/admin.py
+++ W3/ADMIN_DJANGO/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categoria, Produto
 
-# Register your models here.
+@admin.register(Categoria)
+class CategoriaAdimin(admin.ModelAdmin):
+    pass
+
+@admin.register(Produto)
+class ProdutoAdimin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'descricao', 'categoria')
+    list_filter = ('categoria',)
+    list_editable = ('preco', )
+    list_display_links = ('nome', 'descricao')
+    search_fields = ('nome', 'descricao')
+    ordering  = ['nome', 'preco', 'descricao', 'categoria']
--- W3/ADMIN_DJANGO/core/models.py
+++ W3/ADMIN_DJANGO/core/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
-# Create your models here.
-
 class Categoria(models.Model):
     nome = models.CharField(max_length=20)
 
+    def __str__(self) -> str:
+        return self.nome
+
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
-    preco = models.ImageField()
+    imagem = models.ImageField(null=True)
+    preco = models.IntegerField()
     descricao = models.TextField()
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.nome
