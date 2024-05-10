21e863e7618f096ba465dd5bcb8ebec5ef84a7c7
OsmarsProgramador <123673084+OsmarsProgramador@users.noreply.github.com>
2024-05-10 00:39:48 -0300

    A

--- Stilo_Low_code/choperia/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Produto, Categoria
-
-"""admin.site.register(Categoria)
-admin.site.register(Produto)"""
-
--- Stilo_Low_code/minhachoperia/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("choperia/", include("choperia.urls")),
-    path("admin/", admin.site.urls),
-]
