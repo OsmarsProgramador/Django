9b8d81c0b61502d4895ce3421a4626ab1112214b
OsmarsProgramador <123673084+OsmarsProgramador@users.noreply.github.com>
2024-06-12 22:14:04 -0300

    A

--- MinhaChoperia/app_choperia/urls.py
+++ MinhaChoperia/app_choperia/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index", views.index, name="index"),
+    path("index/", views.index, name="index"),
     path('home/', views.home, name = 'home'),
     path('tabela_produtos/', views.tabela_produtos, name='tabela_produtos'),
     path('cadastrar_produto/', views.cadastrar_produto, name='cadastrar_produto'),
