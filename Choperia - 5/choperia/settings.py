e33ed449159bb9ba0ef3a115eedabde15b30b894
OsmarsProgramador <123673084+OsmarsProgramador@users.noreply.github.com>
2024-10-02 04:04:40 -0300

    Modal traca usuario

--- Choperia - 5/choperia/settings.py
+++ Choperia - 5/choperia/settings.py
@@ -131,13 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Configuração para WhiteNoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Media files (Uploaded files) tem como objetivo mapear o caminho para imagens
